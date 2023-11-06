bfcf4a7d07ca2d70ed65fed55ce69dc27d75b81c
the_byte_bender <cccefg2@gmail.com>
2023-11-06 18:17:44 +0300

    fix: Start buffering again if the jitter buffer empties

--- app/remote_user.py
+++ app/remote_user.py
@@ -18,7 +18,7 @@ class RemoteUser(Thread):
         self.decoder = OpusDecoder()
         self.decoder.set_sampling_frequency(48000)
         self.decoder.set_channels(2)
-        self.source = context.gen_source(direct_channels = True)
+        self.source = context.gen_source(direct_channels=True)
         self.lock = RLock()
         self.running = True
         self.start()
@@ -28,20 +28,23 @@ class RemoteUser(Thread):
         if self.buffering and len(self.chunks) >= 3:
             self.buffering = False
 
+    def get_chunk(self):
+        chunk = self.chunks.pop()
+        if not self.chunks and not self.buffering:
+            self.buffering = True
+
     def update(self):
         if self.buffering or not self.chunks:
             return
         with self.lock:
-            chunk = self.chunks.popleft()
+            chunk = self.get_chunk()
             if self.source.buffers_processed:
                 buffer = self.source.unqueue_buffers(max=1)[0]
             elif self.source.buffers_queued < 3:
                 buffer = self.context.gen_buffer()
             else:
                 return
-            buffer.set_data(
-                chunk, sample_rate=48000, format=cyal.BufferFormat.STEREO16
-            )
+            buffer.set_data(chunk, sample_rate=48000, format=cyal.BufferFormat.STEREO16)
             self.source.queue_buffers(buffer)
             if self.source.state in [
                 cyal.SourceState.STOPPED,
@@ -53,6 +56,7 @@ class RemoteUser(Thread):
         while self.running:
             self.update()
             time.sleep(0.001)
+
     def destroy(self):
         self.running = False
         self.join()
