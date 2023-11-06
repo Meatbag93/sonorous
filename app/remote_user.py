627d1366cc120c4551b2f726463209adad0732ac
the_byte_bender <cccefg2@gmail.com>
2023-11-07 01:50:41 +0300

    feat: Allow setting the jitter buffer size as an argument, per remote user

--- app/remote_user.py
+++ app/remote_user.py
@@ -8,13 +8,20 @@ import cyal
 class RemoteUser(Thread):
     """Represents another connected user"""
 
-    def __init__(self, context: cyal.Context, id: int, display_name: str):
+    def __init__(
+        self,
+        context: cyal.Context,
+        id: int,
+        display_name: str,
+        jitter_buffer_size: int = 3,
+    ):
         """Construct a new RemoteUser. You must call destroy() to dispose of this object otherwise a memory leak happens."""
         super().__init__(name=f"RemoteUser-{id}-{display_name}", daemon=True)
         self.context = context
         self.id = id
         self.display_name = display_name
-        self.chunks = deque(maxlen=3)
+        self.jitter_buffer_size = jitter_buffer_size
+        self.chunks = deque(maxlen=jitter_buffer_size)
         self.buffering = True
         self.decoder = OpusDecoder()
         self.decoder.set_sampling_frequency(48000)
@@ -26,7 +33,7 @@ class RemoteUser(Thread):
 
     def put_packet(self, packet: bytes):
         self.chunks.append(bytes(self.decoder.decode(memoryview(bytearray(packet)))))
-        if self.buffering and len(self.chunks) >= 3:
+        if self.buffering and len(self.chunks) >= self.jitter_buffer_size:
             self.buffering = False
 
     def get_chunk(self):
@@ -39,7 +46,7 @@ class RemoteUser(Thread):
         with self.lock:
             if self.source.buffers_processed:
                 buffer = self.source.unqueue_buffers(max=1)[0]
-            elif self.source.buffers_queued < 3:
+            elif self.source.buffers_queued < self.jitter_buffer_size:
                 buffer = self.context.gen_buffer()
             else:
                 return
